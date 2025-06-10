b0a53983ff0c6c6c8e41ecc400a77d8ee22b00dd
judeik <judeojobor@gmail.com>
2025-06-10 23:12:33 +0100

    Fix: Updated Drug APIs

--- index.js
+++ index.js
@@ -94,9 +94,57 @@ app.get("/drugs/prescription", (req, res)=>{
 app.post("/drugs/manufacturer-count", (req, res)=>{
     const { manufacturer } = req.body
 
+    if(!manufacturer){
+        return res.status(400).json({message: "Please add a manufacturer"})
+    }
+
     const manufacturerCount = drugs.filter((each)=>{
         return each.manufacturer == manufacturer
     })
 
-    res.json({count: manufacturerCount.length, manufacturerCount})
+    res.status(200).json({
+        message: "Success",
+        count: manufacturerCount.length, manufacturerCount
+    })
 })
+
+// Status Codes
+/**
+ * 100, 101 ------ Informational Status code 
+ * 
+ * 200 ------ If it is successful
+ * 
+ * 201 ----- If it is successful and something was created or added into a database, you use 201. All the status codes that falls within 200 is used when something is successful
+ * 
+ * 300, 301 ----- Either a page, files or resuorces has moved somewhere. Is for redirect, something has been moved somewhere. It is a redirect and moved resources
+ * 
+ * 400: --- Bad request from the user. When a user make a bad request you return him back with status code of 400.
+ * 
+ * 401: ----- The user did not signin of did not authuenicated
+ * 
+ * 404: --- What they are looking does not exist
+
+* 500: ----- Any issue coming from your server (Backend). i.e. server error
+
+** After you add a status code it is very important you add a message 
+
+*** As a backend developer there are two important things you must do.
+1. CRUD => Create, Read, Update and Delete something from the database.
+
+2. Authentication: Register a user, Login a user, Forgot password, Reset password and Send them an Email. 
+
+******************
+DATABASE
+Two types of Database:
+1. Relational database: Are those databases that save its data in a table. e.g. MySQL, Postgres. They works with tables and every tables has rows and columns. One user data will be in one row and another is second row etc.
+
+2. Non Relational Database: E.g. Mango 
+Mango Database is a non relational database because it doesn't save data in a table rather it saves in a document. e.g. a notebook, you open the first page and write the information the user, you open the second page and write the details of the second user. No two users must be written in the same page. You use a doucument for every user. And all these users you put the documents together and it becomes a collection.
+
+********
+Is there any Database to use?
+
+** If you are working on an application that you knows everything about it, you should go MySQL or PostGress But if it is a database that the software application can grow at any time go for Mongo database. Mango Database can easily grow with any project.
+Stooped: 20:00
+**/
+
